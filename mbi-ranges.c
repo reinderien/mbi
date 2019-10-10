20acc3e5fedebc70d97f1ea2229ee8bd9c643a02
gtoombs <gtoombs@encircleapp.com>
2019-10-10 16:06:49 -0400

    Clean up some junk

--- mbi-ranges.c
+++ mbi-ranges.c
@@ -1,67 +1,76 @@
 #include <assert.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define N 11
 
 static const char
     *prog,
     symbols[] = "0123456789ACDEFGHJKMNPQRTUVWXYZ",
     numbers[] = "0123456789",
     *const letters = symbols + 10,
-    *const digits[] = {
+    *const digits[N] = {
         numbers + 1,
         letters, symbols, numbers,
         letters, symbols, numbers,
         letters, letters, numbers, numbers,
     };
 static const unsigned
-    N = sizeof(digits)/sizeof(*digits),
-    NNUMBERS = sizeof(numbers) - 1,
-    NSYMBOLS = sizeof(symbols) - 1,
-    NLETTERS = sizeof(symbols) - sizeof(numbers),
-    BUFFER_SIZE = (10000 / (N+1)) * (N+1),
-    sizes[] = {
-        NNUMBERS - 1,
-        NLETTERS, NSYMBOLS, NNUMBERS,
-        NLETTERS, NSYMBOLS, NNUMBERS,
-        NLETTERS, NLETTERS, NNUMBERS, NNUMBERS,
-    };
+    BUFFER_SIZE = (10000 / (N+1)) * (N+1);
+static unsigned sizes[N];
+static unsigned long facs[N];
 static char *buffer, *bufp;
+
+static void init() {
+    unsigned long fac = 1;
+    for (int i = N-1; i > 0; i--) {
+        sizes[i] = strlen(digits[i]);
+        facs[i] = fac;
+        fac *= sizes[i];
+    }
+    
+    buffer = malloc(BUFFER_SIZE);
+    assert(buffer);
+    bufp = buffer;
+}
 
 static void usage() {
     fprintf(stderr, "Usage: %s starting-code count\n", prog);
     exit(EINVAL);
 }
 
-static void parse(const char *src, unsigned dest[N]) {
-    for (int i = 0; i < N; i++) {
+static void parse(const char *src, unsigned code[N], unsigned long within_digit[N]) {
+    unsigned long sum = 0;
+    for (int i = N-1; i >= 0; i--) {
         const char *found = strchr(digits[i], src[i]);
         if (!found) {
             fprintf(stderr, "Invalid character '%c' in code string position %d\n", src[i], i);
             usage();
         }
-        dest[i] = found - digits[i];
+        
+        unsigned d = found - digits[i];
+        code[i] = d;
+        within_digit[i] = sum;
+        sum += d*facs[i];
     }
 }
 
-static void increment(unsigned code[N]) {
-    for (int p = N-1; p >= 0; p--) {
-        if (++code[p] < sizes[p])
-            return;
-        if (p == 0) {
-            fprintf(stderr, "Won't increment past last valid code\n");
-            exit(E2BIG);
+static void print_and_incr(unsigned code[N], unsigned long within_digit[N]) {
+    for (int i = N-1; i >= 0; i--) {
+        bufp[i] = digits[i][code[i]];
+        if (++within_digit[i] >= facs[i]) {
+            within_digit[i] = 0;
+            if (++code[i] >= sizes[i])
+                code[i] = 0;
         }
-        code[p] = 0;
     }
-}
-
-static void print_buffer(const unsigned code[N]) {
-    for (int i = 0; i < N; i++)
-        *bufp++ = digits[i][code[i]];
-    *bufp++ = '\n';
+    bufp += N;
+    *bufp = '\n';
+    bufp++;
 }
 
 static void bflush() {
@@ -75,8 +84,11 @@ int main(int argc, const char **argv) {
     if (argc != 3)
         usage();
     
+    init();
+    
     unsigned code[N];
-    parse(argv[1], code);
+    unsigned long within_digit[N];
+    parse(argv[1], code, within_digit);
     
     unsigned long count;
     if (sscanf(argv[2], "%lu", &count) != 1) {
@@ -84,16 +96,10 @@ int main(int argc, const char **argv) {
         usage();
     }
     
-    buffer = malloc(BUFFER_SIZE);
-    assert(buffer);
-    bufp = buffer;
-    
     for (unsigned long i = 0; i < count; i++) {
-        print_buffer(code);
+        print_and_incr(code, within_digit);
         if (bufp >= buffer + BUFFER_SIZE)
             bflush();
-        
-        increment(code);
     }
     
     bflush();
